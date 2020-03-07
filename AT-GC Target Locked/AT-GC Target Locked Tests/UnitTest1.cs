f2f761468699e3f5af4f7150dcb9a363d24f4648
Bernard Allotey <benard.allotey4@gmail.com>
2020-03-07 14:13:36 -0600

    Added another function to the Annotation Interface

---
+++ AT-GC Target Locked/AT-GC Target Locked Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AT_GC_Target_Locked_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
