2ae9d781aa0bcbdfde7c13ffd3f9aea2d8d9ca8f
Bernard Allotey <benard.allotey4@gmail.com>
2020-03-07 15:09:40 -0600

    Added a base PubTator Response class

--- AT-GC Target Locked/AT-GC Target Locked/Models/Interfaces/IAnnotation.cs
+++ AT-GC Target Locked/AT-GC Target Locked/Models/Interfaces/IAnnotation.cs
@@ -7,13 +7,13 @@ namespace AT_GC_Target_Locked.Models.Interfaces
     /**
      * This interface is a base class for any annotation
      */
-    public interface IAnnotation
+    public abstract class IAnnotation
     {
         public string Id { get; set; }
         public Dictionary<string, string> Infons { get; set; }
         public string Text { get; set; }
         public List<Location> Locations { get; set; }
-        public IAnnotation ParseString(string json);
+        public abstract IAnnotation ParseString(string json);
     }
 
     public struct Location
