8ae794ba254fd9b7dd62579bed77df2011280254
Bernard Allotey <benard.allotey4@gmail.com>
2020-03-07 16:44:31 -0600

    Finished ResponseTest

--- AT-GC Target Locked/AT-GC Target Locked Tests/ResponseTests.cs
+++ AT-GC Target Locked/AT-GC Target Locked Tests/ResponseTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using AT_GC_Target_Locked.Io;
 using AT_GC_Target_Locked.Models;
+using AT_GC_Target_Locked.Models.PubTator;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AT_GC_Target_Locked_Tests
@@ -31,6 +33,20 @@ namespace AT_GC_Target_Locked_Tests
                                                               "        \"locations\": [\n            {\n           " +
                                                               "   \"offset\": 19,\n              \"length\": 21\n      " +
                                                               "      }\n          ]\n        }").Infons["type"]);
+        }
+
+        [TestMethod]
+        public void EnsureCorrectWholeResponseLogic()
+        {
+            var text = System.IO.File.ReadAllText("testResponse.json");
+            var response = PubTatorResponse.ParseString(text);
+            Assert.AreEqual("20085714", response.Id);
+            Assert.IsTrue(response.Authors.Any(s => s.Contains("Halfter")));
+            Assert.AreEqual(2010, response.Year);
+            Assert.IsTrue(response.Created.Equals(DateTimeOffset.FromUnixTimeMilliseconds(1572526582545)
+                .DateTime));
+            Assert.IsTrue(response.Passages.Any(passage => passage.Offset == 99));
+            Assert.IsTrue(response.Tags.Contains(Tag.Disease));
         }
     }
 }
