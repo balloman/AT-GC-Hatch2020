b8b62845db764ac3080c587f022df18d126f5110
Bernard Allotey <benard.allotey4@gmail.com>
2020-03-07 11:40:52 -0600

    Initial Project Commit

---
+++ AT-GC Target Locked/AT-GC Target Locked/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AT_GC_Target_Locked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
