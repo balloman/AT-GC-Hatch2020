5621279ea63074cb8a939e7da251979455c26eec
Bernard Allotey <benard.allotey4@gmail.com>
2020-03-07 12:59:50 -0600

    Preliminary API Call code, idk if this works

---
+++ AT-GC Target Locked/AT-GC Target Locked/Io/PubTatorHandler.cs
@@ -0,0 +1,48 @@
+﻿//Created by Bernard Allotey at 12:48 PM Satruday
+
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using AT_GC_Target_Locked.Models;
+
+namespace AT_GC_Target_Locked.Io
+{
+    /**
+     * This class should be singleton, so only instantiated once, handles the WebIO
+     */
+    public class PubTatorHandler
+    {
+        private HttpClient Client { get; }
+
+        private const string BaseAddress =
+            "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocjson"; 
+
+        // ReSharper disable once InconsistentNaming
+        private static readonly PubTatorHandler INSTANCE = new PubTatorHandler();
+
+        
+        private PubTatorHandler()
+        {
+            Client = new HttpClient {BaseAddress = new Uri(BaseAddress)};
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        /**
+         * Use this to grab the PubTatorHandler Instance
+         */
+        public PubTatorHandler GetInstance()
+        {
+            return INSTANCE;
+        }
+
+        public IArticleResponse GetArticleByPmId(string id)
+        {
+            var response = Client.GetAsync("?pmids=" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+            return null;
+        }
+    }
+}
---
+++ AT-GC Target Locked/AT-GC Target Locked/Models/Interfaces/IArticleResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace AT_GC_Target_Locked.Models
+{
+    public interface IArticleResponse
+    {
+        public string GetTitle();
+        public string GetAbstract();
+    }
+}
