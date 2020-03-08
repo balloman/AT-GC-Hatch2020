3b65ad96bd1f3b7f82369ec87fa1109f42b1d068
Riley Cook <riley.the.cook@gmail.com>
2020-03-07 19:08:59 -0600

    yay tests succeed now

--- AT-GC Target Locked/AT-GC Target Locked/Models/PubTator/PubTatorResponse.cs
+++ AT-GC Target Locked/AT-GC Target Locked/Models/PubTator/PubTatorResponse.cs
@@ -21,6 +21,7 @@ namespace AT_GC_Target_Locked.Models.PubTator
         public PubTatorResponse()
         {
             Infons = new Dictionary<string, string>();
+            Passages = new List<Passage>();
             Tags = new List<Accessor>();
             Authors = new List<string>();
         }
@@ -45,19 +46,27 @@ namespace AT_GC_Target_Locked.Models.PubTator
         {
             JObject jsonAnnotation = JObject.Parse(json);
             var response = new PubTatorResponse();
-            response.Id = jsonAnnotation.GetValue("Id").ToObject<String>();
-            response.Infons = jsonAnnotation.GetValue("Infons").ToObject<Dictionary<string,string>>();
+            response.Id = jsonAnnotation.GetValue("id").ToObject<String>();
+            response.Infons = jsonAnnotation.GetValue("infons").ToObject<Dictionary<string,string>>();
             var passagesArray = JArray.Parse(jsonAnnotation["passages"].ToString());
             foreach (var passage in passagesArray)
             {
                 var passageObject = new Passage();
-                passageObject.Infons = passage.ToObject<Dictionary<String, String>>();
-                passageObject.Offset = passage.ToObject<int>();
-                passageObject.Text = passage.ToObject<String>();
-                // call annotations
-                
+                passageObject.Infons = passage["infons"].ToObject<Dictionary<String, String>>();
+                passageObject.Offset = passage["offset"].ToObject<int>();
+                passageObject.Text = passage["text"].ToObject<String>();
+                var annotationsListString = passage["annotations"].ToString();
+                var annotationsListArray = JArray.Parse(annotationsListString);
+                passageObject.Annotations = new List<Annotation>();
+                foreach (var token in annotationsListArray)
+                {
+                    passageObject.Annotations.Add(Annotation.ParseString(token.ToString()));
+                }
+                response.Passages.Add(passageObject);
             }
-            var created = DateTimeOffset.FromUnixTimeMilliseconds(jsonAnnotation.GetValue("$date").ToObject<int>()).DateTime;
+
+            var date = jsonAnnotation.GetValue("created");
+            response.Created = DateTimeOffset.FromUnixTimeMilliseconds(jsonAnnotation.GetValue("created")["$date"].ToObject<long>()).DateTime;
             var tagsArray = JArray.Parse(jsonAnnotation["accessions"].ToString());
             foreach (var token in tagsArray)
             {
@@ -70,6 +79,10 @@ namespace AT_GC_Target_Locked.Models.PubTator
                     tag = myTag, data = parts[1]
                 });
             }
+            var authorsArray = jsonAnnotation.GetValue("authors").ToObject<List<String>>();
+            response.Authors = authorsArray;
+            response.Journal = jsonAnnotation.GetValue("journal").ToObject<String>();
+            response.Year = jsonAnnotation.GetValue("year").ToObject<int>();
             return response;
         }
     }
